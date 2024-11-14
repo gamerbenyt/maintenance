0a3027daeae169f5f220016acf41c28e5979b5ac
Gamer Ben <136914442+gamerbenyt@users.noreply.github.com>
2024-11-14 15:34:16 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Coming Soon to a Browser Near You</title>		
+		<link rel="stylesheet" type="text/css" href="style.css" />
+		
+		<script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js'></script>
+		<script type='text/javascript' src='javascripts/jquery.tipsy.js'></script>
+		<script type='text/javascript'>
+		$(function() {
+			$('#tipsy').tipsy({fade: true, gravity: 's'});
+		});
+		</script>
+		<!--[if IE]>
+		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		
+	</head>
+	<body>
+		<div class="wrapper">
+			<img src="images/logo.png" title="Akardoo Network" style="width:20%; height:20%;"/>
+<br>
+</br>
+<br>
+</br>
+			<img src="images/construccion.png" align="left" style="width:38%; height:70%;"/>
+			<h1>Maintenance!</h1>
+
+			<p>Unfortunatly, we are under a maintenance to improve everything. <strong> But for now you can see our progress:</strong></p>
+</br>
+</br>
+</br>
+<br>
+						
+			<section class="progress">
+				<div class="progress-bar-container" id="tipsy" title="35% Completado"> <!-- Edit this title for the tooltip pop up -->
+					<article class="progress-bar" style="width:35%"  ></article> <!-- Edit the width percentage value to indicate progress -->
+				</div>
+<!------------------- Article-------->
+			</section>
+<div class="clear"></div>		</div>
+<div class="footer">
+	<div class="footer-container">
+    	
+    	<ul class="footer-menu">
+            <li class="footer-menu-title">Useful links</li>
+            <li><a href="http://donar.akardoo.com">Donate</a></li>
+            <li><a href="#">Forums</a></li>
+            <li><a href="#">Sponsors</a></li>
+        </ul>        
+        
+    	<div class="footer-dummy"></div>
+        
+      	<div class="footer-data">
+        	All rights reserved 2014 &copy; Akardoo Network<br> 
+       		<div class="footer-links">
+       			<a href="#">About us</a>  /  <a href="#">Contact</a>  /  <a href="#">ToS</a>
+       		</div>
+        	<a href="https://twitter.com/intent/follow?original_referer=http%3A%2F%2Fakardoo.com%2F&region=follow_link&screen_name=Akardoo&tw_p=followbutton&variant=2.0" target="_blank" class="follow-twitter">Follow @Akardoo</a>
+
+        	<a href="http://fb.akardoo.com" target="_blank" class="follow-facebook">Follow @OficialAkardoo</a>
+
+        </div>
+    
+    </div>	</body>
+</html>
